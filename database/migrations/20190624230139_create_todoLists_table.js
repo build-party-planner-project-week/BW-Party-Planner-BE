a9e951d5de1894541b6e597bfe4f60f8789154c8
Tarmstrong95 <triston95strong@gmail.com>
2019-06-24 23:25:21 -0400

    Make todoLists table

---
+++ database/migrations/20190624230139_create_todoLists_table.js
@@ -0,0 +1,25 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('todoLists', tbl => {
+        tbl
+        .increments()
+  
+        tbl
+        .string('todo')
+        .notNullable()
+
+        tbl
+        .string('price')
+        .notNullable()
+  
+        tbl
+        .integer('party_id')
+        .unsigned()
+        .references('id')
+        .inTable('parties')
+    })
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('todoLists')
+  };
+  
