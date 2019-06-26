ed52747dbce7e32fb63eadbed0607f57a5a7847c
Tarmstrong95 <triston95strong@gmail.com>
2019-06-26 00:33:29 -0400

    update bug in migration drop table

--- database/migrations/20190624230105_create_entertainments_table.js
+++ database/migrations/20190624230105_create_entertainments_table.js
@@ -17,5 +17,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('entertainment')
+  return knex.schema.dropTable('entertainments')
 };
