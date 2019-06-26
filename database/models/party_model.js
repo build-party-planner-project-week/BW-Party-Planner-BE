aa79bddf73760cee151e765a30e05a2b6c4d6b34
Tarmstrong95 <triston95strong@gmail.com>
2019-06-25 23:07:30 -0400

    Add party model database methods

--- database/models/party_model.js
+++ database/models/party_model.js
@@ -1,44 +1,129 @@
 const db = require('../dbConfig')
 
 module.exports = {
-findParty,
-findEnter,
-findImage,
-findtodos,
-findshoplist
+    findParty,
+    findEnter,
+    findImage,
+    findtodos,
+    findshoplist,
+    postparty,
+    postenter,
+    postimg,
+    posttodo,
+    postshop,
+    putparty,
+    putenter,
+    putimg,
+    puttodo,
+    putshop,
+    deletparty,
+    deletenter,
+    deletimg,
+    delettodo,
+    deletshop
 }
 
 function findParty(userID) {
-return db("users")
-.join("parties", "parties.user_id", "users.id")
-.select("parties.id", "parties.guests", "parties.theme", "parties.date", "parties.budget")
-.where({user_id: userID})
+    return db("parties")
+        .where({ user_id: userID })
 }
 
 function findEnter(partyID) {
-    return db('parties')
-    .join('entertainments', "entertainments.party_id", "parties.id")
-    .select("*")
-    .where({party_id: partyID})
+    return db('entertainments')
+        .where({ party_id: partyID })
 }
 
-function findImage(partyID){
-    return db('parties')
-    .join('images', "images.party_id", "parties.id")
-    .select("*")
-    .where({party_id: partyID})
+function findImage(partyID) {
+    return db('images')
+        .where({ party_id: partyID })
 }
 
-function findtodos(partyID){
-    return db('parties')
-    .join('todoLists', "todoLists.party_id", "parties.id")
-    .select("*")
-    .where({party_id: partyID})
+function findtodos(partyID) {
+    return db('todoLists')
+        .where({ party_id: partyID })
 }
 
-function findshoplist(partyID){
+function findshoplist(partyID) {
+    return db('shopLists')
+        .where({ party_id: partyID })
+}
+
+
+
+
+function postparty(party) {
     return db('parties')
-    .join("shopLists", "shopLists.party_id", "parties.id")
-    .select("*")
-    .where({party_id: partyID})
+        .insert(party, 'id')
+}
+function postenter(enter) {
+    return db('entertainments')
+        .insert(enter, 'id')
+}
+function postimg(image) {
+    return db('images')
+        .insert(image, 'id')
+}
+function posttodo(todo) {
+    return db('todoLists')
+        .insert(todo, 'id')
+}
+function postshop(item) {
+    return db('shopLists')
+        .insert(item, 'id')
+}
+
+
+
+function putparty(party, id) {
+    return db('parties')
+        .where({ id: id })
+        .update(party, 'id')
+}
+function putenter(enter, id) {
+    return db('entertainments')
+        .where({ id: id })
+        .update(enter)
+}
+function putimg(image, id) {
+    return db('images')
+        .where({ id: id })
+        .update(image)
+}
+function puttodo(todo, id) {
+    return db('todoLists')
+        .where({ id: id })
+        .update(todo)
+}
+function putshop(item, id) {
+    return db('shopLists')
+        .where({ id: id })
+        .update(item)
+}
+
+
+
+function deletparty(id) {
+    return db('parties')
+        .where({ id: id })
+        .del()
+}
+function deletenter(id) {
+    return db('entertainments')
+        .where({ id: id })
+        .del()
+}
+function deletimg(id) {
+    return db('images')
+        .where({ id: id })
+        .del()
+}
+function delettodo(id) {
+    return db('todoLists')
+        .where({ id: id })
+        .del()
+}
+function deletshop(id) {
+    return db('shopLists')
+        .where({ id: id })
+        .del()
 }
